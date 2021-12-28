93d5331e9c469ead5f62fa175a5158682e780ffa
Valentinas <vkornijenka@gmail.com>
2021-12-28 20:37:48 +1000

    added basic calculator logic

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let numEntry = "";
+let firstNum = "";
+let secondNum = "";
+let currentOperator = "";
 let storedNum = "";
-document.getElementById("current-num").innerHTML = `${numEntry}`;
+document.getElementById("current-num").innerHTML = `${firstNum}`;
 document.getElementById("previous-num").innerHTML = `${storedNum}`;
 
 //look into data numbers for HTML
 //look into text content for HTML to create loop for number input
 
 const nums = document.querySelectorAll("button.number");
+const operators = document.querySelectorAll("button.operator");
 const ac = document.getElementById("ac");
 const del = document.getElementById("del");
 const division = document.getElementById("division");
@@ -17,21 +20,36 @@ const equal = document.getElementById("equal");
 const multiplier = document.getElementById("multiplier");
 
 nums.forEach((num) =>
-  num.addEventListener("click", () => numButtonCreator(num.textContent))
+  num.addEventListener("click", () => numInput(num.textContent))
 );
 
-function numButtonCreator(num) {
-  numEntry += num;
-  displayNum();
+operators.forEach((operator) =>
+  operator.addEventListener("click", () => operatorInput(operator.textContent))
+);
+
+
+//fix below, non functioning calculator
+function operatorInput(operator) {
+  if (operator == "+") {
+    currentOperator += operator;
+    displayNum();
+  } else if (operator == "=") {
+    operate(firstNum, secondNum, operator);
+  }
 }
 
-// function zeroAdd(num) {
-//   numEntry += num;
-//   displayNum();
-// }
-
+function numInput(num) {
+  if (firstNum < "0") {
+    firstNum += num;
+    displayNum();
+  } else {
+    secondNum += num;
+    displayNum();
+  }
+}
+//fix above 
 function displayNum() {
-  document.getElementById("current-num").innerHTML = `${numEntry}`;
+  document.getElementById("current-num").innerHTML = `${firstNum}${currentOperator}${secondNum}`;
   document.getElementById("previous-num").innerHTML = `${storedNum}`;
 }
 
