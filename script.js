9ca3a2aa4c5866acb282c166a1f1975e963b0421
Valentinas <vkornijenka@gmail.com>
2022-01-28 15:59:26 +1000

    added append function

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let displayValue = "";
-let storedValue = "";
-let operation = undefined;
-
 const numberButtons = document.querySelectorAll("[data-number]");
 const operatorButtons = document.querySelectorAll("[data-operator]");
 const acButton = document.querySelector("[data-ac]");
@@ -10,29 +6,38 @@ const equalButton = document.querySelector("[data-equal]");
 const previousNumberText = document.querySelector("[data-previous-number]");
 const currentNumberText = document.querySelector("[data-current-number]");
 
+let displayValue = "";
+let storedValue = "";
+let operation = undefined;
+
+function clear() {
+  displayValue = "";
+  storedValue = "";
+  operation = undefined;
+}
+
+function append(number) {
+  if (number === '.' && displayValue.includes('.')) return;
+  displayValue = displayValue + number;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  currentNumberText.innerText = displayValue;
+}
+
 numberButtons.forEach((num) =>
-  num.addEventListener("click", () => numInput(num.textContent))
+  num.addEventListener("click", () => {
+    append(num.innerText);
+    updateDisplay();
+  })
 );
 
 operatorButtons.forEach((operator) =>
-  operator.addEventListener("click", () => operatorInput(operator.textContent))
+  operator.addEventListener("click", () => {
+    
+  })
 );
-
-
-
-function operatorInput(operator) {
-  return currentOperator+=operator, displayNum();
-}
-
-function numInput(num) {
-  return displayValue += num, displayNum();
-}
-
-
-function displayNum() {
-  document.getElementById("current-num").innerHTML = `${displayValue}`;
-  document.getElementById("previous-num").innerHTML = `${storedNum}`;
-}
 
 function add(a, b) {
   return a + b;
